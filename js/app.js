8564c818a9c81dd18cda2778fc36277976cc8b1b
obaedulislam <obaed.cse@gmail.com>
2022-09-04 01:16:23 +0600

    Catgories Display Dynamically

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+/*==| Fetch Data From API Section Start |==*/
+const loadAllCategories = async() => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.data.news_category;
+}
+/*==| Fetch Data From API Section End |==*/
+
+/*==| Display All Categories Section Start |==*/
+const displayAllCategories = async () => {
+    const categories = await loadAllCategories();
+    console.log(categories)
+    const categoriesContainer = document.getElementById('categories-menu');
+    categories.forEach(category => {
+        console.log(category);
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+            <a class="text-decoration-none active text-secondary px-3 py-2" href="#">${category.category_name}</a>
+        `;
+        categoriesContainer.appendChild(categoryDiv);
+    });
+}
+displayAllCategories();
+/*==| Display All Categories Section End |==*/
+loadAllCategories();
